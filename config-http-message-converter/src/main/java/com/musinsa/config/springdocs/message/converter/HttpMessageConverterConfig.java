5a8a9860d612f832956e1b6537ee466bc1e11a03
neunghyun <ropeze@gmail.com>
2026-01-13 04:02:12 +0900

    전역 API 메시지 변환을 위한 HttpMessageConverter 설정 추가

---
+++ config-http-message-converter/src/main/java/com/musinsa/config/springdocs/message/converter/HttpMessageConverterConfig.java
@@ -0,0 +1,19 @@
+package com.musinsa.config.springdocs.message.converter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class HttpMessageConverterConfig {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(objectMapper);
+        converter.setDefaultCharset(StandardCharsets.UTF_8);
+        return converter;
+    }
+}
